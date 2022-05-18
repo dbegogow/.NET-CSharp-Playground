0e78bca0bd5f7816206e299553d6c9f742a7167a
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-05-18 18:24:25 +0300

    Create client and change gRPC server logic

--- CSharpCommunication/gRPC/gRPCClient/Program.cs
+++ CSharpCommunication/gRPC/gRPCClient/Program.cs
@@ -1,10 +1,22 @@
-﻿//using Grpc.Net.Client;
-//using gRPCServer;
+﻿using Grpc.Net.Client;
+using gRPCServer;
 
-//var chanel = GrpcChannel.ForAddress("https://localhost:7290");
-//var client = new Greeter.GreeterClient(chanel);
+var chanel = GrpcChannel.ForAddress("https://localhost:7290");
+var client = new Greeter.GreeterClient(chanel);
 
-//var greet = new HelloRequest { Name = "Dzhulio" };
-//var reply = await client.SayHelloAsync(greet);
+var newGreetingRequests = new List<GreetingRequest>
+{
+    new GreetingRequest {Name = "Ivana", Country = "France"},
+    new GreetingRequest {Name = "Ivana", Country = "Italy"},
+    new GreetingRequest {Name = "Dzhulio", Country = "Germany"},
+};
 
-//Console.WriteLine(reply.Message);
+foreach (var newGreeting in newGreetingRequests)
+{
+    var isAddedSuccessfuly = await client.SaveGreetingAsync(newGreeting);
+    Console.WriteLine($"Saving new greeting status: {isAddedSuccessfuly.Summary}");
+}
+
+var allGreetings = await client.GetGreetingsAsync(new Empty());
+
+Console.WriteLine(string.Join(Environment.NewLine, allGreetings.Replies));
--- CSharpCommunication/gRPC/gRPCServer/Services/GreeterService.cs
+++ CSharpCommunication/gRPC/gRPCServer/Services/GreeterService.cs
@@ -4,32 +4,46 @@ namespace gRPCServer.Services;
 
 public class GreeterService : Greeter.GreeterBase
 {
-    private readonly HashSet<GreetingRequest> requests;
+    private readonly List<GreetingRequest> requests;
 
     public GreeterService()
     {
-        this.requests = new HashSet<GreetingRequest>();
+        this.requests = new List<GreetingRequest>();
     }
 
     public override Task<StatusResponse> SaveGreeting(GreetingRequest request, ServerCallContext context)
     {
-        var isSuccessful = this.requests.Add(request); ;
+        var isNameExist = this.IsNameExist(request.Name);
 
         var statusResponse = new StatusResponse();
 
-        if (isSuccessful)
+        if (isNameExist)
         {
             statusResponse.Summary = "Error: This name already exists.";
         }
-
-        statusResponse.Summary = "Success: Name is added successfully.";
+        else
+        {
+            this.requests.Add(request);
+            statusResponse.Summary = "Success: Name is added successfully.";
+        }
 
         return Task.FromResult(statusResponse);
     }
 
-    public override Task<IEnumerable<GreetingReply>> GetGreetings(Empty request, ServerCallContext context)
-        => Task.FromResult(this.requests.Select(r => new GreetingReply
-        {
-            Message = $"Hello from {r.Country}. My name is {r.Name}"
-        }));
+    public override Task<GreetingReplies> GetGreetings(Empty request, ServerCallContext context)
+    {
+        var greetings = this.requests
+            .Select(r => new GreetingReply
+            {
+                Message = $"Hello from {r.Country}. My name is {r.Name}"
+            });
+
+        var greetingsResponse = new GreetingReplies();
+        greetingsResponse.Replies.AddRange(greetings);
+
+        return Task.FromResult(greetingsResponse);
+    }
+
+    private bool IsNameExist(string name)
+        => this.requests.Any(r => r.Name == name);
 }
