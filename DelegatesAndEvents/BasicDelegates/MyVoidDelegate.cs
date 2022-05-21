c883524a690df2ec6b47e8260da7388b9d3b51c9
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-05-21 17:52:22 +0300

    Upload code from basic delegates project

---
+++ DelegatesAndEvents/BasicDelegates/MyVoidDelegate.cs
@@ -0,0 +1,7 @@
+﻿namespace BasicDelegates;
+
+public delegate void MyVoidDelegate(int number);
+
+public delegate void MyVoidDelegateWithString(string text);
+
+public delegate string MyCoolDelegate(int first, int second);
--- DelegatesAndEvents/BasicDelegates/Program.cs
+++ DelegatesAndEvents/BasicDelegates/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace BasicDelegates;
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public void SomePersonMethod(int number)
+    {
+
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        //MyVoidDelegate voidDelegate = PrintInteger;
+
+        var voidDelegate = new MyVoidDelegate(PrintInteger);
+
+        voidDelegate += SomeMethod;
+        voidDelegate += PrintInteger;
+
+        var person = new Person();
+
+        voidDelegate += person.SomePersonMethod;
+
+        //voidDelegate -= SomeMethod;
+
+        //voidDelegate += x => Console.WriteLine("Inline func.");
+        //voidDelegate += x => Console.WriteLine("Inline func 2.");
+        //voidDelegate += x => Console.WriteLine("Inline func 3.");
+
+        voidDelegate?.Invoke(10);
+        //voidDelegate?.DynamicInvoke(10); // When you don't know the type of the delegate
+
+        //voidDelegate(100);
+
+        Console.WriteLine("---------");
+
+        Console.WriteLine(voidDelegate?.Target?.GetType().Name);
+    }
+
+    public static void PassSomeDelegate(MyVoidDelegate del)
+    {
+        del(5);
+    }
+
+    public static void PrintInteger(int number)
+    {
+        Console.WriteLine(number);
+    }
+
+    public static void SomeMethod(int myInt)
+    {
+        Console.WriteLine(myInt + 10);
+    }
+
+    public static void SomeMethodWithString(string text)
+    {
+        Console.WriteLine(text);
+    }
+
+    public static string SomeOtherMethod(int x, int y)
+        => x + y.ToString();
+}
