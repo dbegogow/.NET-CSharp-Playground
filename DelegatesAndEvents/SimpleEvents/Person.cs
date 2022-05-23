37f24077170691c294100bfdf5727631577f202f
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-05-23 17:41:26 +0300

    Create simple events project

---
+++ DelegatesAndEvents/SimpleEvents/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleEvents
+{
+    public class Person
+    {
+        private int health;
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Health
+        {
+            get => this.health;
+            set
+            {
+                this.health = value;
+                this.OnHealthChanged?.Invoke(this, this.health);
+            }
+        }
+
+        public event EventHandler<int> OnHealthChanged;
+    }
+}
---
+++ DelegatesAndEvents/SimpleEvents/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SimpleEvents;
+
+public class Program
+{
+    public static void Main()
+    {
+        var person = new Person
+        {
+            Id = 1,
+            Name = "Dzhulio",
+            Health = 100
+        };
+
+        person.OnHealthChanged += PersonOnHealthChanged;
+        person.OnHealthChanged += PersonIsDead;
+
+        person.Health = 200;
+
+        for (int i = 0; i < 10; i++)
+        {
+            person.Health -= 20;
+        }
+    }
+
+    private static void PersonIsDead(object sender, int health)
+    {
+        var person = (Person)sender;
+        if (person.Health <= 0)
+        {
+            Console.WriteLine($"{person.Name} is no longer alive");
+        }
+    }
+
+    private static void PersonOnHealthChanged(object sender, int health)
+    {
+        var person = (Person)sender;
+        Console.WriteLine($"{person.Name} has new health: {health}");
+    }
+}
