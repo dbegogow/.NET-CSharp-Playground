17452ba96cdaffd55208904a8d5ffbd80732d46c
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-05-28 14:17:30 +0300

    Create Guest model

---
+++ CSharpToCSharpCommunication/Refit/SimpleApi/Models/GuestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleApi.Models
+{
+    public class GuestModel
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
