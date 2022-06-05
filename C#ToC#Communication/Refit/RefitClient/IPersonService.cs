9fa070f2862c0a5b85dfd139701c0c18644272bc
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-06-05 16:49:58 +0300

    Create refit IPersonService interface

---
+++ C#ToC#Communication/Refit/RefitClient/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RefitCommon;
+
+using Refit;
+
+namespace RefitClient;
+
+public interface IPersonService
+{
+    [Get("/api/person")]
+    Task GetAll();
+
+    [Post("/api/person")]
+    Task<IEnumerable<Person>> AddPerson([Body] Person person);
+}
--- C#ToC#Communication/Refit/RefitClient/Program.cs
+++ C#ToC#Communication/Refit/RefitClient/Program.cs
@@ -4,6 +4,6 @@ public class Program
 {
     public static async Task Main()
     {
-        Console.WriteLine("Yes");
+        
     }
 }
