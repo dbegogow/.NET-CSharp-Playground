12034070775cb273d430148612ad742169a90015
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-06-05 15:09:37 +0300

    Create RefitCommon Class Library for Person model

---
+++ C#ToC#Communication/Refit/RefitCommon/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace RefitCommon;
+
+public class Person
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public DateTime Birthday { get; set; }
+}
---
+++ C#ToC#Communication/Refit/RefitServer/Controllers/PersonController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RefitServer.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PersonController : ControllerBase
+{
+    private static readonly List<>
+}
