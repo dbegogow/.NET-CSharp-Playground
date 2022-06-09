37a6cca9895910a62c98e135c2a01676678f95d7
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-06-09 12:50:28 +0300

    Create User model

---
+++ Dapper/MinimalAPIDapperDemo/DataAccess/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DataAccess.Models;
+
+internal class UserModel
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+}
