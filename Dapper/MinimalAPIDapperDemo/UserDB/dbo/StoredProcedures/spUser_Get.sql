376642aad3d2fde36ff7f05d7a296f8c675d8be9
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-06-09 10:51:20 +0300

    Create stored procedures for getting all users and only one

---
+++ Dapper/MinimalAPIDapperDemo/UserDB/dbo/StoredProcedures/spUser_Get.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Get]
+	@Id int
+AS
+BEGIN
+	SELECT *
+	FROM dbo.[User]
+	WHERE Id = @Id
+END
---
+++ Dapper/MinimalAPIDapperDemo/UserDB/dbo/StoredProcedures/spUser_GetAll.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[spUser_GetAll]
+AS
+BEGIN
+	SELECT *
+	FROM dbo.[User];
+END
