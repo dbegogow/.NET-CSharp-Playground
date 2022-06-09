49bce8da451a3265a80d0f656705a6ab84922b3d
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-06-09 14:07:55 +0300

    Implement get particular user method

--- Dapper/MinimalAPIDapperDemo/DataAccess/Data/UserData.cs
+++ Dapper/MinimalAPIDapperDemo/DataAccess/Data/UserData.cs
@@ -14,4 +14,13 @@ public class UserData
 
     public Task<IEnumerable<UserModel>> GetUsers()
         => this._db.LoadData<UserModel, dynamic>("dbo.spUser_GetAll", new { });
+
+    public async Task<UserModel> GetUser(int id)
+    {
+        var result = await this._db.LoadData<UserModel, dynamic>(
+            "dbo.spUser_Get",
+            new { Id = id });
+
+        return result.FirstOrDefault();
+    }
 }
