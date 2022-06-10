45080eac201f124623a9ce03b25216f974915e65
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-06-10 11:18:05 +0300

    Create GlobalUsings cs file

--- Dapper/MinimalAPIDapperDemo/MinimalAPIDapperDemo/Program.cs
+++ Dapper/MinimalAPIDapperDemo/MinimalAPIDapperDemo/Program.cs
@@ -1,7 +1,11 @@
+using DataAccess.DbAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+builder.Services.AddSingleton<IUserData, IUserData>();
 
 var app = builder.Build();
 
