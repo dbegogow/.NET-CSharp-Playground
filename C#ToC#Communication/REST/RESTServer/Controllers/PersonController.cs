8f6e088a63fb8e1094ee6dee157c47cc80610764
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-06-11 00:06:54 +0300

    Create simple Person controller with get and post methods

---
+++ C#ToC#Communication/REST/RESTServer/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using RESTCommon;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTServer.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PersonController : ControllerBase
+{
+    private static readonly List<Person> People = new()
+    {
+        new () {Id = 1, Name = "Dzhulio", Age = 19 },
+        new () {Id = 2, Name = "Ivana", Age = 20 },
+        new () {Id = 3, Name = "Ivaylo", Age = 32 }
+    };
+
+    [HttpGet]
+    public IActionResult Get() => Ok(People);
+
+    [HttpPost]
+    public IActionResult Post(Person person)
+    {
+        if (string.IsNullOrWhiteSpace(person.Name)
+            || person.Age <= 0)
+        {
+            return BadRequest();
+        }
+
+        People.Add(person);
+
+        return Ok();
+    }
+}
