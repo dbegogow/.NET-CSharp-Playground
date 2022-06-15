4f8039ac65cd313162bf087005253f8163864ffc
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-06-15 18:44:31 +0300

    Implement extension method that redirect to action with compile time checks

---
+++ ExpressionTrees/WebAppExpressions/Controllers/AnotherController.cs
@@ -0,0 +1,13 @@
+﻿using WebAppExpressions.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppExpressions.Controllers
+{
+    public class AnotherController : Controller
+    {
+        public IActionResult About()
+        {
+            return this.RedirectTo<HomeController>(c => c.Index());
+        }
+    }
+}
---
+++ ExpressionTrees/WebAppExpressions/Infrastructure/ControllerExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppExpressions.Infrastructure
+{
+    public static class ControllerExtensions
+    {
+        public static IActionResult RedirectTo<TController>(
+            this Controller controller,
+            Expression<Action<TController>> redirectExpression)
+        {
+            if (redirectExpression.Body.NodeType != ExpressionType.Call)
+            {
+                throw new InvalidOperationException($"The provided expression is not a valid method call: {redirectExpression.Body}");
+            }
+
+            var methodCallExpression = (MethodCallExpression)redirectExpression.Body;
+
+            var actionName = methodCallExpression.Method.Name;
+            var controllerName = typeof(TController).Name.Replace(nameof(controller), string.Empty);
+
+            return controller.RedirectToAction(actionName, controllerName);
+        }
+    }
+}
