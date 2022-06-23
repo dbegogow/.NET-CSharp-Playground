358aa4c2182f6559f561131da6ec6201b0ebd654
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-06-23 22:02:28 +0300

    Create project for super fast property getters with expression trees

---
+++ ExpressionTrees/PropertyHelperWithExpressions/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+namespace PropertyHelperWithExpressions;
+
+public class Program
+{
+    public static void Main()
+    {
+        // return RedirectToAction("Index", new { id = 5, query = "Text" });
+        // Dictionary { ["id"] = 5, ["query"] = "Text" }
+
+        var obj = new { id = 5, query = "Text" };
+
+        var dict = new Dictionary<string, object>();
+
+        var stopwatch = Stopwatch.StartNew();
+
+        for (int i = 0; i < 1_000_000; i++)
+        {
+            obj
+                .GetType()
+                .GetProperties()
+                .Select(pr => new
+                {
+                    pr.Name,
+                    Value = pr.GetValue(obj)
+                })
+                .ToList()
+                .ForEach(pr => dict[pr.Name] = pr.Value ?? string.Empty);
+        }
+
+        Console.WriteLine($"{stopwatch.Elapsed} - Reflection Property Getters");
+        Console.WriteLine(dict.Count);
+
+        dict = new Dictionary<string, object>();
+
+        stopwatch = Stopwatch.StartNew();
+
+        for (int i = 0; i < 1_000_000; i++)
+        {
+            PropertyHelper
+                .GetProperties(obj.GetType())
+                .Select(pr => new
+                {
+                    pr.Name,
+                    Value = pr.Getter(obj)
+                })
+                .ToList()
+                .ForEach(pr => dict[pr.Name] = pr.Value);
+        }
+
+        Console.WriteLine($"{stopwatch.Elapsed} - Expression Tree Getters");
+        Console.WriteLine(dict.Count);
+    }
+}
---
+++ ExpressionTrees/PropertyHelperWithExpressions/PropertyHelper.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq.Expressions;
+using System.Collections.Concurrent;
+
+namespace PropertyHelperWithExpressions;
+
+public class PropertyHelper
+{
+    private static readonly Type TypeOfObject = typeof(object);
+    private static ConcurrentDictionary<Type, PropertyHelper[]> cache
+        = new ConcurrentDictionary<Type, PropertyHelper[]>();
+
+    public string Name { get; set; }
+
+    public Func<object, object> Getter { get; set; }
+
+    public static PropertyHelper[] GetProperties(Type type)
+        => cache.GetOrAdd(type, _ =>
+        {
+            return type
+            .GetProperties()
+            .Select(pr =>
+            {
+                // Object obj
+                var parameter = Expression.Parameter(TypeOfObject, "obj");
+
+                // (T)obj
+                var parameterConvert = Expression.Convert(parameter, type);
+
+                // ((T)obj).Property
+                var body = Expression.MakeMemberAccess(parameterConvert, pr);
+
+                // (object)((T)obj).Property
+                var convertedBody = Expression.Convert(body, TypeOfObject);
+
+                // Object obj => (object)((T)obj).Property
+                var lambda = Expression
+                    .Lambda<Func<object, object>>(convertedBody, parameter);
+
+                var propertyGetterFunc = lambda.Compile();
+
+                return new PropertyHelper
+                {
+                    Name = pr.Name,
+                    Getter = propertyGetterFunc
+                };
+            })
+            .ToArray();
+        });
+}
