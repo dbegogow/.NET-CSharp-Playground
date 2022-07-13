59a62bb8e664be7e3162e1782acf58ac9d1a0778
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-07-13 18:12:40 +0300

    Implement calculate factorial with recursion

---
+++ RecursionPrograms/CalculateFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CalculateFactorial;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter the number: ");
+        var number = int.Parse(Console.ReadLine());
+
+        var fact = CalculateFactorial(number);
+
+        Console.WriteLine($"Factorial from given number: {fact}");
+    }
+
+    private static int CalculateFactorial(int number)
+    {
+        if (number == 1)
+        {
+            return 1;
+        }
+
+        return number * CalculateFactorial(number - 1);
+    }
+}
