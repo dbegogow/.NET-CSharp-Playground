2c8fe41a368c625e3786a29b5ac06054931c19c0
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-07-14 17:56:01 +0300

    Implement sum digits number problem with recursion

---
+++ RecursionPrograms/NumberDigitsSum/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace NumberDigitsSum;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter the number: ");
+        var number = int.Parse(Console.ReadLine());
+
+        var sum = CalculateDigitsSum(number);
+
+        Console.WriteLine($"Number sum of digits: {sum}");
+    }
+
+    private static int CalculateDigitsSum(int number)
+    {
+        if (number == 0)
+        {
+            return 0;
+        }
+
+        return number % 10 + CalculateDigitsSum(number / 10);
+    }
+}
