e158e405e7f4825d3c5809e4f388c2e716041c87
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-07-18 08:50:04 +0300

    Implement convert number to binary problem with recursion

---
+++ RecursionPrograms/ConvertIntegerToBinary/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConvertIntegerToBinary;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter the number: ");
+        var number = int.Parse(Console.ReadLine());
+
+        BinaryConversion(number);
+    }
+
+    private static int BinaryConversion(int number)
+    {
+        if (number == 0)
+        {
+            return 0;
+        }
+
+        var bin = (number % 2) + 10 * BinaryConversion(number / 2);
+        Console.Write(bin);
+
+        return 0;
+    }
+}
