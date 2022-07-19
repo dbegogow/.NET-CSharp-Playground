470a94ac4fc25a90e6b07e62c3abfc90d76ede81
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-07-19 18:36:50 +0300

    Implement calculate power of a number with recursion

---
+++ RecursionPrograms/CalculatePowerOfNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CalculatePowerOfNumber;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter the number: ");
+        var number = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the exponent: ");
+        var exponent = int.Parse(Console.ReadLine());
+
+        var power = CalculatePower(number, exponent);
+
+        Console.WriteLine($"Power is: {power}");
+    }
+
+    private static int CalculatePower(int number, int exponent)
+    {
+        if (exponent < 0)
+        {
+            throw new InvalidOperationException("Invalid exponent");
+        }
+        else if (exponent == 1)
+        {
+            return number;
+        }
+        else if (exponent == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return number * CalculatePower(number, exponent - 1);
+        }
+    }
+}
