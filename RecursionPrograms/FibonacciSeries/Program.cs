77367a0bb4919e25cefa79de619776f425d44de4
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-11-09 23:55:15 +0200

    Implement Fibonacci series problem

---
+++ RecursionPrograms/FibonacciSeries/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FibonacciSeries;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the length of the Fibonacci Series: ");
+        var length = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write(FibonacciSeries(i));
+        }
+    }
+
+    static int FibonacciSeries(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        else if (n == 1)
+        {
+            return 1;
+        }
+
+        return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
+    }
+}
