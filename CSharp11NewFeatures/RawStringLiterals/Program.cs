2a59fccb54d0de02603572b77c94b3e6391b4d65
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-11-22 19:28:50 +0200

    Write demo code for raw string literals - new feature in C# 11

---
+++ CSharp11NewFeatures/RawStringLiterals/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+var sb = new StringBuilder();
+
+var xmlPrologue = """<?xml version="1.0" encoding="UTF-8"?>""";
+var xmlPrologue1 = """"<?xml version="""1.0" encoding="UTF-8"?>"""";
+var xmlPrologue2 = $"""<?xml version="{1.0m}" encoding="UTF-8"?>""";
+
+var name = "Dzhulio Begogov";
+
+var jsonText = $$""""
+        {
+            "name": "{{name}}"
+        }
+        """";
+
+sb.AppendLine(xmlPrologue);
+sb.AppendLine(xmlPrologue1);
+sb.AppendLine(xmlPrologue2);
+sb.AppendLine(jsonText);
+
+Console.WriteLine(sb.ToString());
