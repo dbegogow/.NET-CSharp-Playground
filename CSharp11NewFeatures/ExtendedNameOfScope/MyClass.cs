082da3ef5f125b9678a93e2154a40df9688cafaf
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-11-22 20:09:34 +0200

    Write demo code for extended name of scope - C# 11 new features

---
+++ CSharp11NewFeatures/ExtendedNameOfScope/MyClass.cs
@@ -0,0 +1,24 @@
+﻿namespace ExtendedNameOfScope;
+
+public class MyClass
+{
+    [Name(nameof(text))]
+    public void Test(string text)
+    {
+        [Name(nameof(T))]
+        void LocalFunction<T>(T param)
+        {
+        }
+
+        var lambdaExpression =
+            ([Name(nameof(aNumber))] int aNumber)
+                => aNumber.ToString();
+    }
+}
+
+public class NameAttribute : Attribute
+{
+    public NameAttribute(string text)
+    {
+    }
+}
