0e001780530338c9f8ce40b94bf55c7aeca50e2d
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-11-23 18:37:08 +0200

    Write demo code for generic math - C# 11 new features

---
+++ CSharp11NewFeatures/GenericMath/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+var numbers = new[] { 1, 2, 3, 4, 5, 0.55 };
+
+var sum = AddAll(numbers);
+
+Console.WriteLine(sum);
+
+T AddAll<T>(T[] values) where T : INumber<T>
+{
+    T result = T.Zero;
+    foreach (var value in values)
+    {
+        result += value;
+    }
+
+    return result;
+}
