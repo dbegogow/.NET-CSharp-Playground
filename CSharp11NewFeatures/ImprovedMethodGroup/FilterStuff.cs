13fcd9dd8a7b235372193c75f2a961a01774077d
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-11-25 16:43:59 +0200

    Write demo code for improved method group conversion to delegate - C# 11 new features

---
+++ CSharp11NewFeatures/ImprovedMethodGroup/FilterStuff.cs
@@ -0,0 +1,15 @@
+﻿namespace ImprovedMethodGroup;
+
+public class FilterStuff
+{
+    private static readonly List<int> Ages = Enumerable.Range(0, 100).ToList();
+
+    public int Sum()
+        => Ages.Where(x => Filter(x)).Sum();
+
+    public int SumMethodGroup()
+        => Ages.Where(Filter).Sum();
+
+    static bool Filter(int age)
+        => age > 50;
+}
