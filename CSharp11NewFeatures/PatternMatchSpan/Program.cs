1c9849df75a38389b594d8fdd6844e850228c6a3
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-11-25 10:44:27 +0200

    Write demo code for pattern matching on spans for constant strings - C# 11 new features

---
+++ CSharp11NewFeatures/PatternMatchSpan/Program.cs
@@ -0,0 +1,11 @@
+﻿ReadOnlySpan<char> text = "Dzhulio Begogov";
+
+if (text is "Dzhulio Begogov")
+{
+    Console.WriteLine("Yes it was");
+}
+
+if (text is ['D', ..])
+{
+    Console.WriteLine("Name starts with D");
+}
