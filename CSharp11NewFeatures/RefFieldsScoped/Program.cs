09dafc67547e691e77527fa313f22a399af85ba1
dbegogow <60520477+dbegogow@users.noreply.github.com>
2022-11-26 14:26:18 +0200

    Write demo code for ref fields and ref scoped variables - C# 11 new features

---
+++ CSharp11NewFeatures/RefFieldsScoped/Program.cs
@@ -0,0 +1,21 @@
+﻿using RefFieldsScoped;
+
+Example();
+
+void Example()
+{
+    var textHolder = new TextHolder(16);
+    Span<char> values = stackalloc char[7] { 'D', 'z', 'h', 'u', 'l', 'i', 'o' };
+    textHolder.Append(values);
+
+    Console.WriteLine(textHolder.ToString());
+}
+
+Span<int> CreateSpan(scoped ref int parameter)
+    => Span<int>.Empty;
+
+Span<int> CreateSpan2()
+{
+    scoped Span<int> span = stackalloc int[420];
+    return Span<int>.Empty;
+}
---
+++ CSharp11NewFeatures/RefFieldsScoped/TextHolder.cs
@@ -0,0 +1,27 @@
+﻿using System.Buffers;
+
+namespace RefFieldsScoped;
+
+public ref struct TextHolder
+{
+    private readonly Span<char> _chars;
+    private int _pos;
+
+    public TextHolder(int size)
+    {
+        this._chars = ArrayPool<char>.Shared.Rent(size);
+        this._pos = 0;
+    }
+
+    public void Append(scoped ReadOnlySpan<char> value)
+    {
+        if (value.TryCopyTo(this._chars.Slice(this._pos)))
+        {
+            this._pos += value.Length;
+        }
+    }
+
+    public override string ToString() => new(Text);
+
+    private ReadOnlySpan<char> Text => this._chars[..this._pos];
+}
