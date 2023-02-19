1b400478c33062ca99b08fa556e5246d108fc61f
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-02-19 12:11:05 +0200

    Create ReflectionDelegatesDemo project

---
+++ Reflection/ReflectionDelegatesDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ReflectionDelegatesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
