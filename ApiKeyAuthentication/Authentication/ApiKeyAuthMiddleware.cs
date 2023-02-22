f96447d7b967ed3ea63e293012e127150291e1ba
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-02-22 08:52:58 +0200

    Implement api key authentication middleware

---
+++ ApiKeyAuthentication/Authentication/ApiKeyAuthMiddleware.cs
@@ -0,0 +1,35 @@
+﻿namespace ApiKeyAuthentication.Authentication;
+
+public class ApiKeyAuthMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly IConfiguration _configuration;
+
+    public ApiKeyAuthMiddleware(RequestDelegate next, IConfiguration configuration)
+    {
+        this._next = next;
+        this._configuration = configuration;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (!context.Request.Headers.TryGetValue(AuthConstants.ApiKeyHeaderName,
+            out var extractedApiKey))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("API Key missing");
+            return;
+        }
+
+        var apiKey = this._configuration.GetValue<string>(AuthConstants.ApiKeySectionName);
+
+        if (!apiKey.Equals(extractedApiKey))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Invalid API Key");
+            return;
+        }
+
+        await this._next(context);
+    }
+}
--- ApiKeyAuthentication/Program.cs
+++ ApiKeyAuthentication/Program.cs
@@ -1,3 +1,5 @@
+using ApiKeyAuthentication.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -13,28 +15,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseMiddleware<ApiKeyAuthMiddleware>();
+
+app.MapControllers();
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
