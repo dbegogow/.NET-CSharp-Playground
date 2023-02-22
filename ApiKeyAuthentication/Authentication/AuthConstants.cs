9795d81e20e85433d02504cffe00b0ce9bd318d0
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-02-22 08:42:24 +0200

    Create ApiKeyAuthentication demo project

---
+++ ApiKeyAuthentication/Authentication/AuthConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiKeyAuthentication.Authentication;
+
+public static class AuthConstants
+{
+    public const string ApiKeySectionName = "Authentication:ApiKey";
+    public const string ApiKeyHeaderName = "X-Api-Key";
+}
