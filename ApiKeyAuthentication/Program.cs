6910877832fc5628ec5a3a3b6a36f47040822e72
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-02-23 17:50:47 +0200

    Add api key auth in the swagger

--- ApiKeyAuthentication/Program.cs
+++ ApiKeyAuthentication/Program.cs
@@ -1,9 +1,37 @@
 using ApiKeyAuthentication.Authentication;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "The API Key to access the API",
+        Type = SecuritySchemeType.ApiKey,
+        Name = "x-api-key",
+        In = ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+    });
+
+    var scheme = new OpenApiSecurityScheme
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+
+    var requirements = new OpenApiSecurityRequirement
+    {
+        {scheme, new List<string>() }
+    };
+
+    x.AddSecurityRequirement(requirements);
+});
 builder.Services.AddAuthorization();
 builder.Services.AddControllers(/*x => x.Filters.Add<ApiKeyAuthFilter>()*/);
 
