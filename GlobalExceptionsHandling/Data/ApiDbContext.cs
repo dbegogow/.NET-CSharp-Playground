d4ee37dc1789b9cbf63cd11459c48e26786ad704
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-04-13 15:51:04 +0300

    Creating the application Db context

---
+++ GlobalExceptionsHandling/Data/ApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GlobalExceptionsHandling.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GlobalExceptionsHandling.Data;
+
+public class ApiDbContext : DbContext
+{
+    public ApiDbContext(DbContextOptions<ApiDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Driver> Drivers { get; set; }
+}
