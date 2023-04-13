f8db35371faeffd328b94340af5f842f22dc1518
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-04-13 15:47:28 +0300

    Create Driver model

---
+++ GlobalExceptionsHandling/Models/Driver.cs
@@ -0,0 +1,10 @@
+﻿namespace GlobalExceptionsHandling.Models;
+
+public class Driver
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int DriverNumber { get; set; }
+}
