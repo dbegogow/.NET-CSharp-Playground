e447ca33b4af2c54a4a59190420c746c4f44503e
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-04-29 12:18:26 +0300

    Create a Driver db model

---
+++ WebApiWithMongoDb/Models/Driver.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebApiWithMongoDb.Models;
+
+public class Driver
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("Name")]
+    public string? DriverName { get; set; } = null;
+
+    public int Number { get; set; }
+
+    public string? Team { get; set; } = null;
+}
