836eccd4ea625e1f9754842b984a24357273c0c0
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-05-13 17:14:28 +0300

    Implement sender code

--- RabbitMQ/SendReceive/Send/Program.cs
+++ RabbitMQ/SendReceive/Send/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+const string message = "Hello from the sender!";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(
+    exchange: string.Empty,
+    routingKey: "hello",
+    basicProperties: null,
+    body: body);
+
+Console.WriteLine($"Sender sent: {message}");
