81e2cdf8b74c82e61f0d756c5ccf1a94d2e4a7a5
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-05-13 21:18:52 +0300

    Create EmitLogs console app for publish logs in the RabbitMq

---
+++ RabbitMQ/PublishSubscribe/EmitLogs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+while (true)
+{
+    Console.Write("Your message: ");
+    var message = Console.ReadLine();
+
+    if (message == null)
+    {
+        Console.WriteLine("Please write a message!");
+        continue;
+    }
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "logs",
+        routingKey: string.Empty,
+        basicProperties: null,
+        body: body);
+
+    Console.WriteLine($"--- Sender sent message: {message}");
+}
