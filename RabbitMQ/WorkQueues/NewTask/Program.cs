b504d462ed7efd58ff82dbb5aaa079b90c53afad
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-05-13 18:10:35 +0300

    Create NewTask console project for the Work Queues

---
+++ RabbitMQ/WorkQueues/NewTask/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: string.Empty,
+                     routingKey: "hello",
+                     basicProperties: null,
+                     body: body);
+
+Console.WriteLine($"--- NewTask send message: {message}");
+
+static string GetMessage(string[] args)
+    => args.Length > 0
+        ? string.Join(" ", args)
+        : "Hello from NewTask!";
