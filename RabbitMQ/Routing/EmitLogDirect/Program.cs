881ddaa7dcdd0f63bb7382de43295261f27e1be5
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-05-13 22:06:02 +0300

    Create EmitLogDirect console project

---
+++ RabbitMQ/Routing/EmitLogDirect/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+
+Console.Write($"Severity: ");
+var severity = Console.ReadLine();
+
+Console.WriteLine(new string('-', 50));
+
+while (true)
+{
+    Console.Write("Your message: ");
+    var message = Console.ReadLine();
+
+    if (message == null)
+    {
+        Console.WriteLine("Please write a message!");
+        continue;
+    }
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "direct_logs",
+        routingKey: severity,
+        basicProperties: null,
+        body: body);
+
+    Console.WriteLine($"--- Sender sent message: {message}");
+}
