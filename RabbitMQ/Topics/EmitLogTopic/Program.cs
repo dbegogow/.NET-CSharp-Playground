1f82a86c7d02400df91036aa6b44dbf8b17e09cc
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-05-17 17:58:55 +0300

    Implement EmitLogTopic project

--- RabbitMQ/Topics/EmitLogTopic/Program.cs
+++ RabbitMQ/Topics/EmitLogTopic/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
+
+Console.Write($"Routing key: ");
+var routingKey = Console.ReadLine();
+
+Console.WriteLine(new string('-', 50));
+
+while (true)
+{
+    Console.Write("Your message: ");
+    var message = Console.ReadLine();
+
+    if (message == null)
+    {
+        Console.WriteLine("Please write a message!");
+        continue;
+    }
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "topic_logs",
+        routingKey: routingKey,
+        basicProperties: null,
+        body: body);
+
+    Console.WriteLine($"--- Sender sent message: {message}");
+}
