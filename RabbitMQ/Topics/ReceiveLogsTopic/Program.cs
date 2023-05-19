7d9bfa668b5373e7eb32d5ad4e1bd89a37191763
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-05-19 23:35:49 +0300

    Create ReceiveLogsTopic console app for RabbitMQ

---
+++ RabbitMQ/Topics/ReceiveLogsTopic/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+Console.Write("Binding keys: ");
+var bindingKeys = Console.ReadLine()
+    .Split(" ")
+    .ToList();
+
+foreach (var bindingKey in bindingKeys)
+{
+    channel.QueueBind(
+        queue: queueName,
+        exchange: "topic_logs",
+        routingKey: bindingKey);
+}
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var routingKey = ea.RoutingKey;
+
+    Console.WriteLine($"Receive message: '{routingKey}':'{message}'");
+};
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
