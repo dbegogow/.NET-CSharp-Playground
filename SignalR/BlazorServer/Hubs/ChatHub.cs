d89c90a9cf2e364e61a2f4b0ae9de1bbdb80d41b
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-06-03 09:46:57 +0300

    Create ChatHub with SendMessage method

---
+++ SignalR/BlazorServer/Hubs/ChatHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorServer.Hubs;
+
+public class ChatHub : Hub
+{
+    public Task SendMessage(string user, string message)
+        => Clients.All.SendAsync("ReceiveMessage", user, message);
+}
