53b003b0df16ae79d63f2d503caf78e219451d62
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-08-19 12:18:32 +0300

    Create project for Ef core compiled queries

---
+++ EFCoreCompiledQueries/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreCompiledQueries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
