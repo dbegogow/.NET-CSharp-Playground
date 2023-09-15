007fe3a34e09a296a7925fb9e654238563f60c07
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-09-15 08:19:08 +0300

    Implement code for using the internal method via reflection

---
+++ Reflection/CoolLibrary/SomeCoolClass.cs
@@ -0,0 +1,12 @@
+﻿namespace CoolLibrary;
+
+internal static class SomeCoolClass
+{
+    internal static string CoolMethod(int number, string text)
+    {
+        Console.WriteLine(number);
+        Console.WriteLine(text);
+
+        return number + text;
+    }
+}
--- Reflection/DynamicReflectionDemo/Program.cs
+++ Reflection/DynamicReflectionDemo/Program.cs
@@ -1,8 +1,19 @@
-﻿namespace DynamicReflectionDemo;
+﻿using System.Reflection;
+
+namespace DynamicReflectionDemo;
 
 public class Program
 {
     public static void Main()
     {
+        var assembly = Assembly.Load(new AssemblyName("CoolLibrary"));
+
+        var type = assembly.GetType("CoolLibrary.SomeCoolClass");
+
+        var method = type.GetMethod("CoolMethod", BindingFlags.NonPublic | BindingFlags.Static);
+
+        var result = (string)method.Invoke(null, new object[] { 42, "some text" });
+
+        Console.WriteLine(result);
     }
 }
