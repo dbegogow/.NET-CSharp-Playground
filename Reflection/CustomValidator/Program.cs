33b25e20a8eae0879ec7a521d6d139a2c52319a0
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-10-17 08:35:48 +0300

    Create CustomValidator with reflection console app for demo code

---
+++ Reflection/CustomValidator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomValidator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
