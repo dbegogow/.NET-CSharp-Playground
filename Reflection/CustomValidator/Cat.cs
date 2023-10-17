f4cd6e368110901929bbe32606392add5a986477
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-10-17 08:45:59 +0300

    Create demo model

---
+++ Reflection/CustomValidator/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomValidator;
+
+public class Cat
+{
+    [Required]
+    [StringLength(20, MinimumLength = 2)]
+    public string Name { get; set; }
+
+    [Range(0, 20)]
+    public int Age { get; set; }
+
+    [StringLength(10)]
+    public string Color { get; set; }
+}
