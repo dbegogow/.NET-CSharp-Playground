15020c09eb1643496405afa03e5343758c65c8c6
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-10-20 00:27:20 +0300

    Get properties and attributes

--- Reflection/CustomValidator/ObjectValidator.cs
+++ Reflection/CustomValidator/ObjectValidator.cs
@@ -1,9 +1,41 @@
-﻿namespace CustomValidator;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace CustomValidator;
 
 public class ObjectValidator
 {
     public ValidationResult Validate(object obj)
     {
+        if (obj == null)
+        {
+            var result = new ValidationResult(false);
 
+            result.Errors.Add("Object", new List<string> { "Object is null." });
+
+            return result;
+        }
+
+        var objectType = obj.GetType();
+
+        var properties = objectType.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance);
+
+        foreach (var property in properties)
+        {
+            var propertyName = property.Name;
+            var propertyValue = property.GetValue(obj);
+
+            var attributes = property.GetCustomAttributes<ValidationAttribute>();
+
+            foreach (var attribute in attributes)
+            {
+                var isValid = attribute.IsValid(propertyValue);
+
+                if (!isValid)
+                {
+                    var errorMessage = attribute.FormatErrorMessage();
+                }
+            }
+        }
     }
 }
