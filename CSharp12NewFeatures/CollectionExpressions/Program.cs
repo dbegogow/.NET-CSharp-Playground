e97ca341f5f6043c01b61e8b572d3796ae985cb1
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-12-02 11:13:22 +0200

    Demo code for Collection expressions

---
+++ CSharp12NewFeatures/CollectionExpressions/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] a = [1, 2, 3, 4, 5, 6, 7, 8];
+
+Span<int> b = ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'i'];
+
+int[][] twoD = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+int[] row0 = [1, 2, 3];
+int[] row1 = [4, 5, 6];
+int[] row2 = [7, 8, 9];
+int[][] twoDFromVariables = [row0, row1, row2];
+int[] normalArrayFromVariables = [.. row0, .. row1, .. row2];
+
+Dictionary<string, int> dict = [];
