84ca0bcc549ea4d48162774b7ff2e8ee4da0925c
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2025-01-11 22:53:54 +0200

    Create new project with C# 13 new features

---
+++ CSharp13NewFeatures/NewLockObject/Program.cs
@@ -0,0 +1,26 @@
+﻿public class LockExampleBefore
+{
+    private readonly object _lock = new();
+
+    public void DoStuff()
+    {
+        lock (_lock)
+        {
+            Console.WriteLine("We're inside old lock");
+        }
+    }
+}
+
+
+public class LockExampleNow
+{
+    private readonly Lock _lock = new();
+
+    public void DoStuff()
+    {
+        lock (_lock)
+        {
+            Console.WriteLine("We're inside new lock");
+        }
+    }
+}
