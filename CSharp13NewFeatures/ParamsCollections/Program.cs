2e1f5969fb7717214db7aa43f8bb7a9a202ca3ae
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2025-01-11 23:36:31 +0200

    Write example for Params collections

---
+++ CSharp13NewFeatures/ParamsCollections/Program.cs
@@ -0,0 +1,13 @@
+﻿// Before
+static void WriteNumbersCount(params int[] numbers)
+   => Console.WriteLine(numbers.Length);
+
+// .NET 9
+static void WriteNumbersCount1(params ReadOnlySpan<int> numbers) =>
+    Console.WriteLine(numbers.Length);
+
+static void WriteNumbersCount2(params IEnumerable<int> numbers) =>
+    Console.WriteLine(numbers.Count());
+
+static void WriteNumbersCount3(params HashSet<int> numbers) =>
+    Console.WriteLine(numbers.Count);
