102e0c27475b5208a4cf5ab71a4dfe70eee780eb
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2025-01-11 22:57:50 +0200

    Write another example for Locking

--- CSharp13NewFeatures/NewLockObject/Program.cs
+++ CSharp13NewFeatures/NewLockObject/Program.cs
@@ -24,3 +24,32 @@ public class LockExampleNow
         }
     }
 }
+
+public class LockExample
+{
+    private readonly Lock _lock = new();
+    private readonly SemaphoreSlim _semaphore = new(1, 1);
+
+    public async Task DoStuff(int val)
+    {
+        lock (_lock)
+        {
+            // await Task.Delay(1000); - Compiler Error: Cannot 'await' in the body of a 'lock' statement
+        }
+
+        using (_lock.EnterScope())
+        {
+            await Task.Delay(1000); // Runtime Error: Instance of type 'System.Threading.Lock.Scope' cannot be preserved across 'await' or 'yield' boundary.
+        }
+
+        await _semaphore.WaitAsync();
+        try
+        {
+            await Task.Delay(10); // No Errors
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+}
