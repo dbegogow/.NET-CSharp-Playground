27f52f5903cb21bde51711e5f739b263463f10fe
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2025-01-11 23:01:03 +0200

    Write example for Task.WhenEach

---
+++ CSharp13NewFeatures/WhenEachTask/Program.cs
@@ -0,0 +1,20 @@
+﻿// List of 5 tasks that finish at random intervals
+var tasks = Enumerable.Range(1, 5)
+   .Select(async i =>
+   {
+       await Task.Delay(new Random().Next(1000, 5000));
+       return $"Task {i} done";
+   })
+   .ToList();
+
+// Before
+while (tasks.Count > 0)
+{
+    var completedTask = await Task.WhenAny(tasks);
+    tasks.Remove(completedTask);
+    Console.WriteLine(await completedTask);
+}
+
+// .NET 9
+await foreach (var completedTask in Task.WhenEach(tasks))
+    Console.WriteLine(await completedTask);
