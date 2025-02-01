1f5aaff90d8c66991b5e722a3efddef9c107fad6
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2025-02-01 15:05:37 +0200

    Write example for Built-in UUID v7 Generation

---
+++ CSharp13NewFeatures/BuiltInUUIDv7Generation/Program.cs
@@ -0,0 +1,10 @@
+﻿var guid = Guid.NewGuid(); // v4 UUID
+Console.WriteLine(guid);
+
+guid = Guid.CreateVersion7(); // v7 UUID
+Console.WriteLine(guid);
+
+Task.Delay(10000).Wait();
+
+guid = Guid.CreateVersion7(TimeProvider.System.GetUtcNow()); // v7 UUID with timestamp
+Console.WriteLine(guid);
