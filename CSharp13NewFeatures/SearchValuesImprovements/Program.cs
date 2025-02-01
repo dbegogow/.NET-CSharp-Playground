54feddbb223300224acf1a192afd061da509432c
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2025-02-01 14:54:43 +0200

    Write example for Search Values improvements

---
+++ CSharp13NewFeatures/SearchValuesImprovements/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Buffers;
+
+var text = "Exploring new capabilities of SearchValues!".AsSpan();
+
+// Before
+var vowelSearch = SearchValues.Create(['n', 'e', 'w']);
+Console.WriteLine(text.ContainsAny(vowelSearch));
+
+// .NET 9
+var keywordSearch = SearchValues.Create(["new", "of"], StringComparison.OrdinalIgnoreCase);
+Console.WriteLine(text.ContainsAny(keywordSearch));
